2a897bf69705d2fc17f7e504281c8e98b5b69de8
KuiraNathan <nathanmwangi70@gmail.com>
2023-03-09 10:18:37 +0300

    Completed assignment

---
+++ let x=10.js
@@ -0,0 +1,29 @@
+let x=10
+
+console.log(x)
+
+let string = "a string"
+let single = 'a string'
+let anotherstring = 'Juma\'s book'
+let doublestring = "Juma's book"
+
+let num = 10
+let aNum = 10.5
+let bNum = 10.5555552456
+
+let theUnknown =undefined
+
+let theNull = null
+
+let boolVal = true//false boolean  true = 1 false = 0
+
+let array = ["string", 10, false, null, undefined]
+
+let obj = {
+    a: "a string",
+    b: 10
+    c: []
+    4: {}
+}
+
+theNull = 
